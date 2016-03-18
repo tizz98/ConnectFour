e0599b16352040e0bea006e49560022f8f68cfc3
tizz98 <dev.tizz98@gmail.com>
2016-03-17 18:09:50 -0700

    lots of work

---
+++ ConnectFour/frmConnectFour.Designer.vb
@@ -0,0 +1,117 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmConnectFour
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnPlayer1 = New System.Windows.Forms.Button()
+        Me.lblPlayer1 = New System.Windows.Forms.Label()
+        Me.lblPlayer2 = New System.Windows.Forms.Label()
+        Me.btnPlayer2 = New System.Windows.Forms.Button()
+        Me.lblWinnerText = New System.Windows.Forms.Label()
+        Me.btnResetBoard = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnPlayer1
+        '
+        Me.btnPlayer1.BackColor = System.Drawing.Color.Aqua
+        Me.btnPlayer1.Font = New System.Drawing.Font("Courier New", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnPlayer1.Location = New System.Drawing.Point(30, 317)
+        Me.btnPlayer1.Name = "btnPlayer1"
+        Me.btnPlayer1.Size = New System.Drawing.Size(70, 70)
+        Me.btnPlayer1.TabIndex = 1
+        Me.btnPlayer1.Text = "O"
+        Me.btnPlayer1.UseVisualStyleBackColor = False
+        '
+        'lblPlayer1
+        '
+        Me.lblPlayer1.AutoSize = True
+        Me.lblPlayer1.Location = New System.Drawing.Point(12, 291)
+        Me.lblPlayer1.Name = "lblPlayer1"
+        Me.lblPlayer1.Size = New System.Drawing.Size(45, 13)
+        Me.lblPlayer1.TabIndex = 2
+        Me.lblPlayer1.Text = "Player 1"
+        '
+        'lblPlayer2
+        '
+        Me.lblPlayer2.AutoSize = True
+        Me.lblPlayer2.Location = New System.Drawing.Point(898, 291)
+        Me.lblPlayer2.Name = "lblPlayer2"
+        Me.lblPlayer2.Size = New System.Drawing.Size(45, 13)
+        Me.lblPlayer2.TabIndex = 4
+        Me.lblPlayer2.Text = "Player 2"
+        '
+        'btnPlayer2
+        '
+        Me.btnPlayer2.BackColor = System.Drawing.Color.Yellow
+        Me.btnPlayer2.Font = New System.Drawing.Font("Courier New", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnPlayer2.Location = New System.Drawing.Point(916, 317)
+        Me.btnPlayer2.Name = "btnPlayer2"
+        Me.btnPlayer2.Size = New System.Drawing.Size(70, 70)
+        Me.btnPlayer2.TabIndex = 3
+        Me.btnPlayer2.Text = "O"
+        Me.btnPlayer2.UseVisualStyleBackColor = False
+        '
+        'lblWinnerText
+        '
+        Me.lblWinnerText.AutoSize = True
+        Me.lblWinnerText.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblWinnerText.Location = New System.Drawing.Point(136, 617)
+        Me.lblWinnerText.Name = "lblWinnerText"
+        Me.lblWinnerText.Size = New System.Drawing.Size(872, 73)
+        Me.lblWinnerText.TabIndex = 5
+        Me.lblWinnerText.Text = "Winner winner chicken dinner"
+        Me.lblWinnerText.Visible = False
+        '
+        'btnResetBoard
+        '
+        Me.btnResetBoard.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnResetBoard.Location = New System.Drawing.Point(288, 704)
+        Me.btnResetBoard.Name = "btnResetBoard"
+        Me.btnResetBoard.Size = New System.Drawing.Size(442, 109)
+        Me.btnResetBoard.TabIndex = 6
+        Me.btnResetBoard.Text = "Reset Board"
+        Me.btnResetBoard.UseVisualStyleBackColor = True
+        '
+        'frmConnectFour
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1033, 843)
+        Me.Controls.Add(Me.btnResetBoard)
+        Me.Controls.Add(Me.lblWinnerText)
+        Me.Controls.Add(Me.lblPlayer2)
+        Me.Controls.Add(Me.btnPlayer2)
+        Me.Controls.Add(Me.lblPlayer1)
+        Me.Controls.Add(Me.btnPlayer1)
+        Me.Name = "frmConnectFour"
+        Me.Text = "Drag 'n Drop Connect Four"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnPlayer1 As Button
+    Friend WithEvents lblPlayer1 As Label
+    Friend WithEvents lblPlayer2 As Label
+    Friend WithEvents btnPlayer2 As Button
+    Friend WithEvents lblWinnerText As Label
+    Friend WithEvents btnResetBoard As Button
+End Class
---
+++ ConnectFour/frmConnectFour.vb
@@ -0,0 +1,206 @@
+﻿Public Class frmConnectFour
+    ' Defaults
+    Private BTN_FONT As New Font("Courier New", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+    Private BTN_SIZE As New Size(70, 70)
+    Private BTN_DROPZONE_STARTING_PT As New Point(160, 35)
+    Private BTN_TOP_ROW_STARTING_PT As New Point(160, 142)
+    Private Const ROW_SPACE As Integer = 76
+    Private Const COL_SPACE As Integer = 103
+    Private Const DROP_ZONE_FMT_STR As String = "btnColumn{0}DropZone"
+    Private Const COL_ROW_FMT_STR As String = "btnCol{0}Row{1}"
+    Private PLAYER_1_COLOR As Color = Color.Aqua
+    Private PLAYER_2_COLOR As Color = Color.Yellow
+    Private OK_DROP_COLOR As Color = Color.Green
+    Private BAD_DROP_COLOR As Color = Color.Red
+    Private Const GO_TEXT As String = ": Go!"
+    Private Const PLAYER_1_TAG As String = "PLAYER_1"
+    Private Const PLAYER_2_TAG As String = "PLAYER_2"
+    Private Const PLAYER_REPR As String = "O"
+
+    Private WithEvents colDropZoneButtons As List(Of Button)
+    Private WithEvents colsRowsButtons As List(Of Button)
+
+    Private Sub frmConnectFour_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        createGameBoard()
+        startNewGame()
+
+        btnPlayer1.Tag = PLAYER_1_TAG
+        btnPlayer2.Tag = PLAYER_2_TAG
+    End Sub
+
+    Private Function getCurrentPlayerTag() As Object
+        Return IIf(btnPlayer1.Enabled, btnPlayer1.Tag, btnPlayer2.Tag)
+    End Function
+
+    Private Sub playerButton_MouseMove(sender As Object, e As MouseEventArgs) Handles btnPlayer1.MouseMove, btnPlayer2.MouseMove
+        If e.Button = MouseButtons.Left Then
+            Dim srcBtn As Button = DirectCast(sender, Button)
+            srcBtn.DoDragDrop(srcBtn.Tag.ToString(), DragDropEffects.All Or DragDropEffects.Link)
+        End If
+    End Sub
+
+    Private Sub dropZoneButton_DragEnter(sender As Object, e As DragEventArgs)
+        ' todo: make sure the column can be dropped on
+        Dim tempBtn As Button = DirectCast(sender, Button)
+
+        If canDropInColumn(CInt(tempBtn.Text)) Then
+            tempBtn.BackColor = OK_DROP_COLOR
+            e.Effect = DragDropEffects.Copy
+        Else
+            tempBtn.BackColor = BAD_DROP_COLOR
+            e.Effect = DragDropEffects.None
+        End If
+    End Sub
+
+    Private Sub dropZoneButton_DragLeave(sender As Object, e As EventArgs)
+        Dim tempBtn As Button = DirectCast(sender, Button)
+        tempBtn.BackColor = DefaultBackColor()
+    End Sub
+
+    Private Sub dropZoneButton_DragDrop(sender As Object, e As DragEventArgs)
+        Dim srcButton As Button = DirectCast(sender, Button)
+        srcButton.BackColor = SystemColors.Control
+
+        Console.Write(e.Data.GetData("Text", True))
+        Console.WriteLine(" in col: " + srcButton.Text)
+
+        dropPlayer(CInt(srcButton.Text), CStr(getCurrentPlayerTag()))
+
+        switchTurns()
+    End Sub
+
+    Private Function canDropInColumn(column As Integer) As Boolean
+        ' basically if the column isn't full; free spaces will have their Text = ""
+        Dim buttonsCol = From btn In colsRowsButtons
+                         Where (DirectCast(btn.Tag, Point)).X = column And btn.Text = ""
+                         Select btn
+        Return buttonsCol.ToArray.Length > 0
+    End Function
+
+    Private Sub dropPlayer(column As Integer, player As String)
+        Dim playerColor As Color = IIf(player = PLAYER_1_TAG, PLAYER_1_COLOR, PLAYER_2_COLOR)
+        Dim buttonsCol = From btn In colsRowsButtons
+                         Where (DirectCast(btn.Tag, Point)).X = column And btn.Text = ""
+                         Order By (DirectCast(btn.Tag, Point)).Y Ascending
+                         Select btn
+
+        Dim buttonsList As List(Of Button) = buttonsCol.ToList()
+
+        For Each btn As Button In buttonsList
+            btn.BackColor = playerColor
+            btn.Text = PLAYER_REPR
+
+            System.Threading.Thread.Sleep(20)
+            Application.DoEvents()
+
+            If Not btn Is buttonsList.Last Then
+                btn.BackColor = DefaultBackColor()
+                btn.Text = ""
+            Else
+                Beep()
+            End If
+        Next
+    End Sub
+
+    Private Sub startNewGame()
+        Dim player1First As Boolean = MessageBox.Show("Should player one go first?", "Start New Game", MessageBoxButtons.YesNo, MessageBoxIcon.Question) = DialogResult.Yes
+        ' reversed so that switchTurns will unreverse it
+        btnPlayer1.Enabled = Not player1First
+        btnPlayer2.Enabled = player1First
+        switchTurns()
+    End Sub
+
+    Private Sub switchTurns()
+        btnPlayer1.Enabled = Not btnPlayer1.Enabled
+        btnPlayer2.Enabled = Not btnPlayer2.Enabled
+
+        If btnPlayer1.Enabled Then
+            lblPlayer1.Text = lblPlayer1.Text & GO_TEXT
+        Else
+            lblPlayer1.Text = lblPlayer1.Text.Replace(GO_TEXT, "")
+        End If
+
+        If btnPlayer2.Enabled Then
+            lblPlayer2.Text = lblPlayer2.Text & GO_TEXT
+        Else
+            lblPlayer2.Text = lblPlayer2.Text.Replace(GO_TEXT, "")
+        End If
+    End Sub
+
+    Private Sub createGameBoard()
+        Dim tempBtn As Button
+        Dim tempPoint As Point
+        Dim colXCoord As Integer
+        Dim colInt As Integer  ' used for naming
+        Dim rowInt As Integer  ' used for naming
+
+        Dim previousTopRowStartingPoint As Point = BTN_TOP_ROW_STARTING_PT
+
+        colDropZoneButtons = New List(Of Button)
+        colsRowsButtons = New List(Of Button)
+
+        ' Create columns
+        For col As Integer = 0 To 6
+            ' Dropzone button first
+            colInt = col + 1
+
+            tempBtn = New Button()
+            tempBtn.Font = BTN_FONT
+
+            ' Y never changes, just like people
+            colXCoord = BTN_DROPZONE_STARTING_PT.X + (col * COL_SPACE)
+            tempPoint = New Point(colXCoord, BTN_DROPZONE_STARTING_PT.Y)
+
+            tempBtn.Location = tempPoint
+            tempBtn.Name = String.Format(DROP_ZONE_FMT_STR, colInt)
+            tempBtn.Size = BTN_SIZE
+            tempBtn.Text = colInt
+            tempBtn.BackColor = DefaultBackColor()  ' for consistency
+            tempBtn.AllowDrop = True
+
+            ' add handlers
+            AddHandler tempBtn.DragEnter, AddressOf dropZoneButton_DragEnter
+            AddHandler tempBtn.DragLeave, AddressOf dropZoneButton_DragLeave
+            AddHandler tempBtn.DragDrop, AddressOf dropZoneButton_DragDrop
+
+            Me.Controls.Add(tempBtn)
+            colDropZoneButtons.Add(tempBtn)
+
+            ' column buttons - start at top and work down
+            For row As Integer = 5 To 0 Step -1
+                rowInt = row + 1
+
+                tempBtn = New Button()
+                tempBtn.Font = BTN_FONT
+
+                ' X never changes (for a column)
+                tempPoint = New Point(colXCoord, BTN_TOP_ROW_STARTING_PT.Y + (row * ROW_SPACE))
+
+                tempBtn.Location = tempPoint
+                tempBtn.Name = String.Format(COL_ROW_FMT_STR, colInt, rowInt)
+                tempBtn.Size = BTN_SIZE
+                tempBtn.Enabled = False
+                tempBtn.Tag = New Point(colInt, rowInt)  ' instead of Text
+
+                Me.Controls.Add(tempBtn)
+                colsRowsButtons.Add(tempBtn)
+            Next
+        Next
+    End Sub
+
+    Private Sub btnResetBoard_Click(sender As Object, e As EventArgs) Handles btnResetBoard.Click
+        For Each dropZoneBtn As Button In colDropZoneButtons
+            Me.Controls.Remove(dropZoneBtn)
+        Next
+
+        For Each colRowBtn As Button In colsRowsButtons
+            Me.Controls.Remove(colRowBtn)
+        Next
+
+        lblPlayer1.Text = lblPlayer1.Text.Replace(GO_TEXT, "")
+        lblPlayer2.Text = lblPlayer2.Text.Replace(GO_TEXT, "")
+
+        createGameBoard()
+        startNewGame()
+    End Sub
+End Class
